9bb3a3928dc75ea72edc659b9ac8a75135e522c2
Dmitry Novik <d1mas1k4@yandex.ru>
2019-10-14 23:47:29 +0300

    Add Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+group = "Make-TCS-Great-Again"
+version = "0.1"
+
+plugins {
+    kotlin("jvm") version "1.3.40"
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
