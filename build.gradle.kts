27adaee5db24109dc7127471bc53c3ac0271d7f5
Dmitry Novik <d1mas1k4@yandex.ru>
2019-10-22 01:17:46 +0300

    Add Coveralls and CodeCov integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.kt3k.gradle.plugin.CoverallsPluginExtension
+
 group = "Make-TCS-Great-Again"
 version = "0.1"
 
 plugins {
     kotlin("jvm") version "1.3.50"
     application
+    jacoco
+    id("com.github.kt3k.coveralls") version "2.8.2"
 }
 
 repositories {
@@ -16,13 +20,28 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
 }
 
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
 application {
     mainClassName = "ru.ifmo.ctd.novik.phylogeny.MainKt"
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = true // coveralls plugin depends on xml format report
+            html.isEnabled = true
+        }
+    }
+
+    coveralls {
+        saveAsFile = true
+        sendToCoveralls = false
+        sourceDirs.add("src/main/kotlin")
+    }
 }
