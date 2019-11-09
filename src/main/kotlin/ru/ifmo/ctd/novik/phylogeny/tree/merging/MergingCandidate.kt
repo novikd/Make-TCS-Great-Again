509e5ffc1e4ec945a692b33133e279ca11407a1d
Novik Dmitry <d1mas1k4@yandex.ru>
2019-11-10 01:53:18 +0300

    Fix EmptyMergingCandidate properties initialization

--- src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/tree/merging/MergingCandidate.kt
+++ src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/tree/merging/MergingCandidate.kt
@@ -1,6 +1,7 @@
 package ru.ifmo.ctd.novik.phylogeny.tree.merging
 
 import ru.ifmo.ctd.novik.phylogeny.common.Cluster
+import ru.ifmo.ctd.novik.phylogeny.common.emptyCluster
 import ru.ifmo.ctd.novik.phylogeny.distance.taxa.TaxonDistanceEvaluator
 import ru.ifmo.ctd.novik.phylogeny.tree.metric.MergeMetric
 
@@ -18,9 +19,9 @@ interface MergingCandidate {
 internal object EmptyMergingCandidate : MergingCandidate {
     override val distance: Int = Int.MAX_VALUE
     override val firstCandidate: Cluster =
-        throw MergingException("Doesn't have first candidate")
+        emptyCluster()
     override val secondCandidate: Cluster =
-        throw MergingException("Doesn't have second candidate")
+        emptyCluster()
 
     override fun merge(evaluator: TaxonDistanceEvaluator, mergeMetric: MergeMetric): Cluster =
         throw MergingException("Can't merge empty candidate")
