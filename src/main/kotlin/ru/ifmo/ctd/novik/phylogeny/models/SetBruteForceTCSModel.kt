888dc39060efa0a7342366fbbb965090f5db171c
Dmitry Novik <Dmitry.Novik@jetbrains.com>
2020-04-13 15:22:40 +0300

    Use BRUTE_FORCE_DISTANCE_THRESHOLD in set brute force mode

--- src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/models/SetBruteForceTCSModel.kt
+++ src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/models/SetBruteForceTCSModel.kt
@@ -4,12 +4,18 @@ import ru.ifmo.ctd.novik.phylogeny.common.Cluster
 import ru.ifmo.ctd.novik.phylogeny.distance.cluster.ClusterDistanceEvaluator
 import ru.ifmo.ctd.novik.phylogeny.tree.merging.SetBruteForceMergingCandidate
 import ru.ifmo.ctd.novik.phylogeny.tree.merging.MergingCandidate
+import ru.ifmo.ctd.novik.phylogeny.tree.merging.SimpleMergingCandidate
+import ru.ifmo.ctd.novik.phylogeny.utils.BRUTE_FORCE_DISTANCE_THRESHOLD
 
 /**
  * @author Dmitry Novik ITMO University
  */
 class SetBruteForceTCSModel(distanceEvaluator: ClusterDistanceEvaluator) : TCSModel(distanceEvaluator) {
     override fun createMergingCandidate(first: Cluster, second: Cluster): MergingCandidate {
-        return SetBruteForceMergingCandidate(first, second, evaluate(first, second))
+        val distance = evaluate(first, second)
+        return if (distance.value > BRUTE_FORCE_DISTANCE_THRESHOLD)
+            SimpleMergingCandidate(first, second, taxonDistanceEvaluator, distance)
+        else
+            SetBruteForceMergingCandidate(first, second, distance)
     }
 }
