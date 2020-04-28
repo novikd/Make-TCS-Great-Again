124e1ca3bc1f3d3f20c89804c77bf4efc430399b
Dmitry Novik <Dmitry.Novik@jetbrains.com>
2020-04-28 13:50:39 +0300

    Disable BruteForceTCSModelTest

--- src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/BruteForceTCSModelTest.kt
+++ src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/BruteForceTCSModelTest.kt
@@ -5,8 +5,8 @@ import ru.ifmo.ctd.novik.phylogeny.utils.PhylogeneticModel
 /**
  * @author Dmitry Novik ITMO University
  */
-internal class BruteForceTCSModelTest : AbstractModelTest() {
-    override val testDirectory: String = "samples"
-    override val outputExtensionPrefix: String = ".bf"
-    override val phylogeneticModel: PhylogeneticModel = PhylogeneticModel.BRUTE_FORCE_TCS
-}
+//internal class BruteForceTCSModelTest : AbstractModelTest() {
+//    override val testDirectory: String = "samples"
+//    override val outputExtensionPrefix: String = ".bf"
+//    override val phylogeneticModel: PhylogeneticModel = PhylogeneticModel.BRUTE_FORCE_TCS
+//}
