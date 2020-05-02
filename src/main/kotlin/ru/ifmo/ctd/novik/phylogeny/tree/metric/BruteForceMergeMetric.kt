b7adbac8b626d84812101ec0be72247cfd38c39d
Dmitry Novik <Dmitry.Novik@jetbrains.com>
2020-05-02 21:59:02 +0300

    Enable BruteForceTCSModelTest

--- src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/tree/metric/BruteForceMergeMetric.kt
+++ src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/tree/metric/BruteForceMergeMetric.kt
@@ -4,10 +4,7 @@ import ru.ifmo.ctd.novik.phylogeny.common.Taxon
 import ru.ifmo.ctd.novik.phylogeny.distance.taxa.TaxonDistanceEvaluator
 import ru.ifmo.ctd.novik.phylogeny.tree.Node
 import ru.ifmo.ctd.novik.phylogeny.tree.merging.MergingMetaData
-import ru.ifmo.ctd.novik.phylogeny.utils.computeDistinctPositions
-import ru.ifmo.ctd.novik.phylogeny.utils.computeGraphDistances
-import ru.ifmo.ctd.novik.phylogeny.utils.permutations
-import ru.ifmo.ctd.novik.phylogeny.utils.toMutableGenome
+import ru.ifmo.ctd.novik.phylogeny.utils.*
 
 open class BruteForceMergeMetric(private val distanceEvaluator: TaxonDistanceEvaluator) : MergeMetric {
 
@@ -16,6 +13,8 @@ open class BruteForceMergeMetric(private val distanceEvaluator: TaxonDistanceEva
     }
 
     override fun compute(firstNode: Node, secondNode: Node, metaData: MergingMetaData): Int {
+        if (firstNode.genome.isEmpty || secondNode.genome.isEmpty)
+            return Int.MIN_VALUE
         val positions = computeDistinctPositions(metaData.firstRealTaxon, metaData.secondRealTaxon)
 
         val firstDistances = firstNode.computeGraphDistances()
--- src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/BruteForceTCSModelTest.kt
+++ src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/BruteForceTCSModelTest.kt
@@ -1,12 +1,23 @@
 package ru.ifmo.ctd.novik.phylogeny.models
 
+import org.junit.jupiter.api.Test
+import ru.ifmo.ctd.novik.phylogeny.base.AbstractTestWithFile
 import ru.ifmo.ctd.novik.phylogeny.utils.PhylogeneticModel
+import ru.ifmo.ctd.novik.phylogeny.utils.create
+import ru.ifmo.ctd.novik.phylogeny.utils.evaluateSimpleData
 
 /**
  * @author Dmitry Novik ITMO University
  */
-//internal class BruteForceTCSModelTest : AbstractModelTest() {
-//    override val testDirectory: String = "samples"
-//    override val outputExtensionPrefix: String = ".bf"
-//    override val phylogeneticModel: PhylogeneticModel = PhylogeneticModel.BRUTE_FORCE_TCS
-//}
+internal class BruteForceTCSModelTest : AbstractTestWithFile() {
+    override val testDirectory: String = "samples"
+    val phylogeneticModel: PhylogeneticModel = PhylogeneticModel.BRUTE_FORCE_TCS
+
+    @Test
+    fun `Brute force doesn't fail`() {
+        runTests {
+            val model = phylogeneticModel.create()
+            model.evaluateSimpleData(this.absolutePath)
+        }
+    }
+}
