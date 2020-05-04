dd6b570c8809945aeece9a7db0f23de9148e5e8c
Dmitry Novik <Dmitry.Novik@jetbrains.com>
2020-05-04 14:40:47 +0300

    Make ChangeRootModification use GlobalRandom

--- src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/mcmc/modifications/ChangeRootModification.kt
+++ src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/mcmc/modifications/ChangeRootModification.kt
@@ -1,6 +1,7 @@
 package ru.ifmo.ctd.novik.phylogeny.mcmc.modifications
 
 import ru.ifmo.ctd.novik.phylogeny.tree.RootedTopology
+import ru.ifmo.ctd.novik.phylogeny.utils.GlobalRandom
 import ru.ifmo.ctd.novik.phylogeny.utils.mergeTwoEdges
 
 class ChangeRootModification : Modification {
@@ -8,7 +9,7 @@ class ChangeRootModification : Modification {
         val recombinationEdges = topology.recombinationGroups.filter { it.isUsed }.flatMap { it.ambassador!!.edges }.toSet()
 
         val oldRoot = topology.root
-        val edgeWithNewRoot = oldRoot.edges.filter { it !in recombinationEdges }.random()
+        val edgeWithNewRoot = oldRoot.edges.filter { it !in recombinationEdges }.random(GlobalRandom)
         val newRoot = topology.getOrCreateNode(edgeWithNewRoot.nodes[1])
 
         val edgeBetweenRoots = newRoot.edges.first { it.end === oldRoot }
