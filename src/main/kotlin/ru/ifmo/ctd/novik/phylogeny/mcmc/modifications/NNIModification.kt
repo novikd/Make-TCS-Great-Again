b0623dcffdf1444b3d11e3494623bb0b6d8d1a03
Novik Dmitry <d1mas1k4@yandex.ru>
2020-10-31 22:56:09 +0300

    WIP on NNI

--- src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/mcmc/modifications/NNIModification.kt
+++ src/main/kotlin/ru/ifmo/ctd/novik/phylogeny/mcmc/modifications/NNIModification.kt
@@ -2,8 +2,7 @@ package ru.ifmo.ctd.novik.phylogeny.mcmc.modifications
 
 import ru.ifmo.ctd.novik.phylogeny.tree.Edge
 import ru.ifmo.ctd.novik.phylogeny.tree.RootedTopology
-import ru.ifmo.ctd.novik.phylogeny.utils.delete
-import ru.ifmo.ctd.novik.phylogeny.utils.logger
+import ru.ifmo.ctd.novik.phylogeny.utils.*
 
 class NNIModification : TreeRearrangement() {
 
@@ -14,13 +13,21 @@ class NNIModification : TreeRearrangement() {
     override fun apply(startEdge: Edge, endEdge: Edge, topology: RootedTopology) {
         log.info { "Chosen edges: $startEdge $endEdge" }
 
-        val startNode = startEdge.start
-        val endNode = endEdge.start
+        val createdStartEdge = getEdgeWithoutReal(startEdge, topology)
+        val createdEndEdge = getEdgeWithoutReal(endEdge, topology)
 
-        startEdge.delete(topology)
-        endEdge.delete(topology)
+        val startNode = createdStartEdge.start
+        val endNode = createdEndEdge.start
 
-        createNewEdge(startNode, endEdge.end, topology)
-        createNewEdge(endNode, startEdge.end, topology)
+        createdStartEdge.delete(topology)
+        createdEndEdge.delete(topology)
+
+        createNewEdge(startNode, createdEndEdge.end, topology)
+        createNewEdge(endNode, createdStartEdge.end, topology)
+
+        topology.mergeTwoEdges(createdStartEdge.end)
+        topology.mergeTwoEdges(createdEndEdge.end)
+
+        debug { topology.checkInvariant() }
     }
 }
