48935a145d6a3a1bc39432c9b5e86511911e81e2
Dmitry Novik <Dmitry.Novik@jetbrains.com>
2020-12-28 22:47:49 +0300

    Reset GLOBAL_RANDOM before each test

--- src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/AbstractModelTest.kt
+++ src/test/kotlin/ru/ifmo/ctd/novik/phylogeny/models/AbstractModelTest.kt
@@ -1,11 +1,14 @@
 package ru.ifmo.ctd.novik.phylogeny.models
 
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import ru.ifmo.ctd.novik.phylogeny.base.AbstractTestWithOutputFile
 import ru.ifmo.ctd.novik.phylogeny.io.output.VerbosePrinter
 import ru.ifmo.ctd.novik.phylogeny.settings.GlobalExecutionSettings
-import ru.ifmo.ctd.novik.phylogeny.utils.*
+import ru.ifmo.ctd.novik.phylogeny.utils.PhylogeneticModel
+import ru.ifmo.ctd.novik.phylogeny.utils.create
+import ru.ifmo.ctd.novik.phylogeny.utils.evaluateSimpleData
+import ru.ifmo.ctd.novik.phylogeny.utils.toGraphviz
 import kotlin.random.Random
 
 /**
@@ -14,10 +17,9 @@ import kotlin.random.Random
 abstract class AbstractModelTest : AbstractTestWithOutputFile() {
     abstract val phylogeneticModel: PhylogeneticModel
 
-    companion object {
-        @BeforeAll @JvmStatic fun setUp() {
-            GlobalExecutionSettings.RANDOM = Random(0)
-        }
+    @BeforeEach
+    fun setUp() {
+        GlobalExecutionSettings.RANDOM = Random(0)
     }
 
     @Test
